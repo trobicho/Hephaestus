08d811d2a42d93684b823e26a2533b72308d5061
trobicho <tomtomy35@gmail.com>
2024-04-15 20:18:55 +0200

    begin MemoryAllocator

--- srcs/memory/hephMemoryAllocator.cpp
+++ srcs/memory/hephMemoryAllocator.cpp
@@ -1,1 +1,7 @@
 #include "hephMemoryAllocator.hpp"
+
+HephResult	HephMemoryAllocator::create(HephDevice& device) {
+	m_device = device;
+
+	return (HephResult());
+}
--- srcs/memory/hephMemoryAllocator.hpp
+++ srcs/memory/hephMemoryAllocator.hpp
@@ -1,6 +1,43 @@
 #pragma once
 
 #include "../core/hephaestus_core.hpp"
+#include <vulkan/vulkan_core.h>
+
+struct	HephImageWrapper {
+	VkImage							image = VK_NULL_HANDLE;
+	VkImageUsageFlags		usage;
+	VkImageLayout				layout = VK_IMAGE_LAYOUT_GENERAL;
+	VkExtent3D					extent = {0, 0, 0};
+	VkFormat						format;
+	VkImageView					imageView = VK_NULL_HANDLE;
+	VkDeviceMemory			memory = VK_NULL_HANDLE;
+	VkDeviceSize				size;
+};
+
+struct	HephBufferWrapper {
+	VkBuffer						buffer = VK_NULL_HANDLE;
+	VkDeviceMemory			memory = VK_NULL_HANDLE;
+	VkBufferUsageFlags	usage = 0;
+	VkDeviceSize				size = 0;
+};
+
+struct	HephBufferCreateInfo {
+	VkDeviceSize						size = 0;
+	VkBufferUsageFlags			usage = 0;
+	VkSharingMode						sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	VkMemoryAllocateFlags		allocateFlags = 0;
+	VkMemoryPropertyFlags		propertyFlags = 0;
+	//deviceMask and pQueue in context (HephMemoryAllocatorCreateInfo )
+};
 
 class		HephMemoryAllocator {
+	public:
+		HephMemoryAllocator() {};
+
+		HephResult	create(HephDevice& device);
+
+		HephResult	allocateBuffer(const HephBufferCreateInfo &createInfo, HephBufferWrapper &buffer);
+
+	private:
+		HephDevice	m_device;
 };
